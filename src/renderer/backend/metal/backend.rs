fc5db78e609d4f7c10c6684f8ee018cba4078547
gituser12981u2 <paullwall@protonmail.com>
2024-08-30 23:10:47 -0700

    fix: ignore render pass creation test

--- src/renderer/backend/metal/backend.rs
+++ src/renderer/backend/metal/backend.rs
@@ -500,8 +500,6 @@ impl<'a> RenderPass<'a> {
 
 #[cfg(test)]
 mod tests {
-    use std::panic;
-
     use super::RenderPass;
     use metal::{Device, MTLViewport};
 
@@ -524,24 +522,8 @@ mod tests {
             zfar: 1.0,
         };
 
-            println!("Creating RenderPass");
-            let render_pass = RenderPass::new(encoder, viewport);
-
-            assert_eq!(render_pass.viewport.width, 800.0);
-            assert_eq!(render_pass.viewport.height, 600.0);
-
-            println!("Test completed successfully")
-        });
-
-        match result {
-            Ok(_) => println!("Test completed without panicking"),
-            Err(e) => {
-                if let Some(s) = e.downcast_ref::<String>() {
-                    println!("Test panicked with message: {s}");
-                } else {
-                    println!("Test panicked without a message");
-                }
-            }
-        }
+        let render_pass = RenderPass::new(encoder, viewport);
+        assert_eq!(render_pass.viewport.width, 800.0);
+        assert_eq!(render_pass.viewport.height, 600.0);
     }
 }
